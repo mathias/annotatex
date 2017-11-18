9774f28480d2e28efe0ae58aee49c0474f77328c
Matt Gauger <matt.gauger@gmail.com>
2017-11-18 00:18:23 -0600

    Just test that the page loads

--- test/annotatex_web/controllers/page_controller_test.exs
+++ test/annotatex_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule AnnotatexWeb.PageControllerTest do
   use AnnotatexWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET /about", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200)
   end
 end
